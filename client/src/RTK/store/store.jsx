db30a33e926aa30841c648fb085ec68898e55aec
Muhammad Usman <usman.latif762@gmail.com>
2023-08-06 05:41:02 +0500

    Budget Tracker Web App

--- client/src/RTK/store/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { mainReducer } from "../slice/Slice";
-
-export const store = configureStore({
-  reducer: { mainSlice: mainReducer },
-});
-
-export default store;
--- client/src/components/helper/ErrorTestDisplayer.jsx
+++
@@ -1,7 +0,0 @@
-import { Typography } from "@mui/material";
-
-
-
-
-export const ErrorText = text => <Typography sx={{ fontSize: '9px', color: 'red', marginBottom: '8px' }}>{text}</Typography>
-
